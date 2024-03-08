15e7b016610fbb1e0d9950ac5d868c0e7c2b33f6
Kristopher Keller <kristopher.h.keller@accenture.com>
2024-03-08 09:34:02 -0700

    Fix broken cluster test

--- internal/provider/cluster_test.go
+++ internal/provider/cluster_test.go
@@ -343,6 +343,7 @@ func TestUnitWaitClusterReady(t *testing.T) {
 
 	cases := []struct {
 		name     string
+		region   *string
 		expected *openapi.DescribeClusterResponseContent
 	}{
 		{
@@ -433,7 +434,7 @@ func TestUnitWaitClusterReady(t *testing.T) {
 	}
 
 	for _, c := range cases {
-		out, err := cR.waitClusterReady(context.TODO(), c.name)
+		out, err := cR.waitClusterReady(context.TODO(), c.name, c.region)
 		if err != nil {
 			if c.name == names["NotFound"] {
 				if err.Error() != failedToFindClusterErr {
@@ -485,16 +486,32 @@ func TestUnitGetCluster(t *testing.T) {
 	}
 	cR.client = openapi.NewAPIClient(c)
 
-	contents := []string{"cluster01", "cluster02"}
+	region := "us-east-1"
+	contents := []struct {
+		name   string
+		region *string
+	}{
+		{
+			name:   "cluster01",
+			region: &region,
+		},
+		{
+			name:   "cluster02",
+			region: nil,
+		},
+	}
 
 	cases := []struct {
-		content  string
+		content struct {
+			name   string
+			region *string
+		}
 		expected openapi.DescribeClusterResponseContent
 	}{
 		{
 			content: contents[0],
 			expected: openapi.DescribeClusterResponseContent{
-				ClusterName:               contents[0],
+				ClusterName:               contents[0].name,
 				ComputeFleetStatus:        openapi.COMPUTEFLEETSTATUS_DISABLED,
 				ClusterStatus:             openapi.CLUSTERSTATUS_CREATE_COMPLETE,
 				CloudFormationStackStatus: openapi.CLOUDFORMATIONSTACKSTATUS_CREATE_COMPLETE,
@@ -503,7 +520,7 @@ func TestUnitGetCluster(t *testing.T) {
 		{
 			content: contents[1],
 			expected: openapi.DescribeClusterResponseContent{
-				ClusterName:               contents[1],
+				ClusterName:               contents[1].name,
 				ComputeFleetStatus:        openapi.COMPUTEFLEETSTATUS_DISABLED,
 				ClusterStatus:             openapi.CLUSTERSTATUS_CREATE_COMPLETE,
 				CloudFormationStackStatus: openapi.CLOUDFORMATIONSTACKSTATUS_CREATE_COMPLETE,
@@ -512,7 +529,7 @@ func TestUnitGetCluster(t *testing.T) {
 	}
 
 	for _, c := range cases {
-		out, err := cR.getCluster(context.TODO(), c.content)
+		out, err := cR.getCluster(context.TODO(), c.content.name, c.content.region)
 		if err != nil {
 			t.Fatalf("Failure while getting cluster: %v", err)
 		}
