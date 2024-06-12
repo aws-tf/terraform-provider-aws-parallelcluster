e4ff95b96ebaded28e628e872142ba0f3937a4dc
Giacomo Marciani <mgiacomo@amazon.com>
2024-06-12 19:55:40 +0200

    [Examples] Consume the aws-parallelcluster provider from Terraform Registry rather than locally.
    Signed-off-by: Giacomo Marciani <mgiacomo@amazon.com>

--- examples/full/resources/aws-parallelcluster_cluster/provider.tf
+++ examples/full/resources/aws-parallelcluster_cluster/provider.tf
@@ -23,7 +23,7 @@ terraform {
   required_version = ">= 1.5.7"
   required_providers {
     aws-parallelcluster = {
-      source  = "terraform.local/local/aws-parallelcluster"
+      source  = "aws-tf/aws-parallelcluster"
       version = "1.0.0-alpha"
     }
   }
